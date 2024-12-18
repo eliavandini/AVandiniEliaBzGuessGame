afd85953d2ac092cd384a73fd32db9f630da681f
vandi <vandini.elia@gmail.com>
2024-12-18 22:53:22 +0100

    writte a basic program structure. migt completly change it by mood

--- src/AVandiniEliaBzGuessGame.java
+++ src/AVandiniEliaBzGuessGame.java
@@ -1,15 +1,234 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Date;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+enum CommandCategory {
+    BASIC,
+    INGAME,
+    SECRET
+}
+
+interface AbstarctAttributes {
+    int getIndex();
+}
+
+enum TextAttributes implements AbstarctAttributes {
+    RESET(0),
+    BRIGHT(1),
+    DIM(2),
+    UNDERSCORE(3),
+    BLINK(4),
+    REVERSE(5),
+    HIDDEN(6);
+
+    private final int index;
+
+    TextAttributes(int index) {
+        this.index = index;
+    }
+    public int getIndex() {
+        return index;
+    }
+}
+
+
+enum FColors implements AbstarctAttributes {
+    BLACK(30),
+    RED(31),
+    GREEN(32),
+    YELLOW(33),
+    BLUE(34),
+    MAGENTA(35),
+    CYAN(36),
+    WHITE(37);
+
+    private final int index;
+
+    FColors(int index) {
+        this.index = index;
+    }
+    public int getIndex() {
+        return index;
+    }
+}
+
+enum BColors implements AbstarctAttributes {
+    BLACK(40),
+    RED(41),
+    GREEN(42),
+    YELLOW(43),
+    BLUE(44),
+    MAGENTA(45),
+    CYAN(46),
+    WHITE(47);
+
+    private final int index;
+
+    BColors(int index) {
+        this.index = index;
+    }
+    public int getIndex() {
+        return index;
+    }
+}
+
+class InputEmptyException extends Exception
+{
+      public InputEmptyException() {}
+
+      public InputEmptyException(String message)
+      {
+         super(message);
+      }
+ }
+
+
+
+
+public class AVandiniEliaBzGuessGame {
+
+    static int highscore = 0;
+    static Command[] comands = new Command[] {};
+    static ArrayList<Game> games = new ArrayList<Game>();
+
+
+
+    public static void main(String[] args) {
+        greeting();
+        newGame();
+
+
+    }
+
+    static void greeting(){
+        System.out.println("Programmed by Vandini Elia");
+    }
+
+    static void rules(){
+        System.out.println("rules");
+    }
+
+    static void winScreen(){
+        System.out.println("Congratulations");
+    }
+
+    static void loosescreen(){
+        System.out.println("Try again");
+    }
+
+    static Game getLastGame(){
+        return games.getLast();
+    }
+
+    static Game newGame(){
+        games.add(new Game());
+        return getLastGame();
+    }
+
+    static void eraseLine(){
+
+    }
+
+    static void eraseLinesUp(int lines){
+
+    }
+
+    static void setAttribute(AbstarctAttributes attr){
+        System.out.printf("\033[%dm", attr.getIndex());
+    }
+
+    static void setAttribute(AbstarctAttributes[] attr){
+        for(AbstarctAttributes a : attr){
+            setAttribute(a);
         }
     }
+
+    static void resetAttrributes(){
+        setAttribute(TextAttributes.RESET);
+    }
+
+    static void clearScreen(){
+        System.out.print("\033[H\033[2J");
+    }
+
+    static void hideCursor() {
+        System.out.println("\033[?25l");
+    }
+
+    static void showCursor() {
+        System.out.println("\033[?25h");
+    }
+}
+
+class Command{
+    String fullName;
+    String command;
+    String shortComand;
+    CommandCategory category;
+    String description;
+    AVandiniEliaBzGuessGame parent;
+
+    public Command(String fullName, String command, String shortComand, CommandCategory category, String description) {
+        this.fullName = fullName;
+        this.command = command;
+        this.shortComand = shortComand;
+        this.category = category;
+        this.description = description;
+    }
+
+    static void exec(String args){
+        return;
+    }
+
+}
+
+class Game{
+
+    char[] code = new char[4];
+    int attempts_left = 20;
+
+    String history = "";
+
+    boolean won = false;
+    boolean lost = false;
+
+    public Game() {
+        this.history = "Game started at " + new Date();
+        while (!lost || !won){
+            execeTurn();
+        }
+    }
+
+    void execeTurn(){
+        parseInput();
+        if (attempts_left <= 0){
+            lost = true;
+        }
+        if (won || lost){
+            finishGame();
+        }
+    }
+
+    String askInput(){
+        System.out.println(attempts_left + ">");
+        return "";
+    }
+
+    void parseInput(){
+        try{
+            String input = askInput();
+            if (input.isEmpty()){
+                throw new InputEmptyException("please input a command or 4 character sequence");
+            }
+        } catch (Exception e){
+
+        }
+    }
+
+    void finishGame(){
+        // change history to include secrect code
+        // save highscore
+    }
+
+
+
 }
